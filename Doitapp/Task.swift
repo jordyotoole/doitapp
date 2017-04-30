d84586bea26f33af6fb65361f1082c90e1e349d1
Jordan O'Toole <jordanotoole@Jordans-Air.gateway>
2017-04-30 21:20:43 +1000

    Doitapp

---
+++ Doitapp/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Doitapp
+//
+//  Created by Jordan O'Toole on 30/4/17.
+//  Copyright © 2017 Jordan O'Toole. All rights reserved.
+//
+
+import Foundation
+
+class Task  {
+    var name = ""
+    var important = false
+}
